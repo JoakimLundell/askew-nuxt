ca626b05bc37e67bc56162652b0b2c6d75f94012
Joakim Lundell <jocke@bluestripes.se>
2025-04-09 11:57:56 +0200

    Add database

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,11 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
+  nitro: {
+    experimental: {
+      database: true,
+      asyncContext: true,
+    },
+  },
+  modules: ["@pinia/nuxt", "nuxt-toast"],
 });
