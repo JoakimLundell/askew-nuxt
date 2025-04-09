81fef422c3cc16aa43c8c68c5a9a2072a8d5f3b6
Joakim Lundell <jocke@bluestripes.se>
2025-04-09 11:58:16 +0200

    Add button

---
+++ components/AskewButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <button>
+    <slot></slot>
+    <IconsLoader v-if="loading" />
+    <template v-else>
+      <IconsLogin v-show="icon === 'login'" />
+      <IconsLogout v-show="icon === 'logout'" />
+    </template>
+  </button>
+</template>
+
+<script setup>
+const props = defineProps({
+  loading: { type: Boolean },
+  icon: { type: String },
+});
+</script>
+
+<style scoped>
+button {
+  border: none;
+  border-radius: 3px;
+  display: flex;
+  align-items: center;
+  gap: 2px;
+  cursor: pointer;
+  background: transparent;
+  color: var(--text);
+}
+</style>
